ed00f1ec4773ea9bdcb7b26d01f0f47d288d9295
J.Tadebois <jpm.tadebois@gmail.com>
2021-03-18 19:29:36 +0400

    Add events for interactive UI elements

--- app/src/main/java/au/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/au/com/example/tipcalculator/MainActivity.kt
@@ -1,7 +1,9 @@
 package au.com.example.tipcalculator
 
 import android.os.Bundle
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
 import au.com.example.tipcalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.etTotal.doAfterTextChanged { text ->
+            TODO("Not yet implemented")
+        }
+
+        binding.sbTipPercentage.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 }
